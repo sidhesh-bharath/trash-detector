7ef5687555de2dd1c20cdd0093095a3fdb4c70c2
sidhesh-bharath <sidheshbharath21@gmail.com>
2025-11-02 17:02:22 +0530

    Added bounding box coordinates(xmin, ymin, xmax, ymax)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 from pathlib import Path
 
-model = YOLO("best.pt")
+model = YOLO("150.pt")
 img_path = Path("image.png")
 output_dir = Path("output")
 output_dir.mkdir(exist_ok=True)
@@ -13,6 +13,17 @@ output_path = output_dir / f"{img_path.stem}-output{img_path.suffix}"
 
 cv2.imwrite(str(output_path), annotated_img)
 print(f"Detection saved to: {output_path}")
+
+for box in results[0].boxes:
+    coords = box.xyxy[0].tolist()
+    conf = float(box.conf[0])*100
+    cls = int(box.cls[0])
+    label = results[0].names[cls]
+    print(f"Detected {label}:")
+    print(f"  - Bounding Box: {coords}")
+    print(f"  - Confidence: {conf:.2f}%\n")
+
+# use list: coords for bounding box coords
 
 cv2.imshow("YOLO Detection Result", annotated_img)
 cv2.waitKey(0)
